28cc7baa4e6c88984609a9a7d2721aa87378d90f
Noor Alrabea <eleanor.alrabea@gmail.com>
2018-03-18 15:21:16 +0100

    Initial Commit

---
+++ AwesomeProjekt/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+class Blink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText: true};
+
+    //Toogle the State every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.isShowingText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
+export default class App extends React.Component {
+  render() {
+    let pic = {
+      uri: 'https://www.anime2you.de/wordpress/wp-content/uploads/2016/12/anime.jpg'
+    };
+    let pic2 = {
+      uri: 'https://t00.deviantart.net/YB1t9p64ZOAAGcpwZmjFR25NM3Y=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/9f64/th/pre/f/2017/031/1/9/jordan_sweeto_by_squchan-daxgdzp.jpg'
+    };
+    return (
+      <View style={styles.container}>
+        <Blink text='Picture 1' />
+        <Image source ={pic} style={{width: 193, height: 110}}/>
+        <Blink text='Picture 2' />
+        <Image source ={pic2} style={{width: 193, height: 110}}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
